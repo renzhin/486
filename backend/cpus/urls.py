ff0f69f894c552c32c03f59ce9a1a1061e987849
Михаил Ренжин <renzhin@gmail.com>
2024-03-01 16:13:30 +0300

    Добавить name и namespace

--- backend/cpus/urls.py
+++ backend/cpus/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'cpus'
+
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
-    path('cpus_list/', views.cpus_list),
-    path('cpu_detail/<int:pk>/', views.cpu_detail),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('cpus_list/', views.cpus_list, name='cpus_list'),
+    path('cpu_detail/<int:pk>/', views.cpu_detail, name='cpu_detail'),
 ]
