ea024576d054cc267d43c8d6190d1a602550a0e0
Михаил Ренжин <renzhin@gmail.com>
2024-03-09 16:52:33 +0300

    Добавить compose + настройки

--- backend/cpu_backend/settings.py
+++ backend/cpu_backend/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Дополнительные директории, где собраны статические файлы проекта.
 STATICFILES_DIRS = [
