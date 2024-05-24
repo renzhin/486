729d04ce7847859171d79e79a7a7dc1136b755ed
Михаил Ренжин <renzhin@gmail.com>
2024-05-24 16:49:42 +0300

    Добавить шаблоны выхода и смены пароля

--- backend/cpu_backend/settings.py
+++ backend/cpu_backend/settings.py
@@ -156,3 +156,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 # Указываем директорию, в которую будут сохраняться файлы писем:
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'cpus:index'
+LOGIN_URL = 'login'
