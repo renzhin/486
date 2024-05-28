9dff5fe793be02c2fec0aef9ef322ca38e09391b
Михаил Ренжин <renzhin@gmail.com>
2024-05-28 17:17:59 +0300

    Добавить путь и шаблон страницы регистрации пользователя

--- backend/cpu_backend/urls.py
+++ backend/cpu_backend/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('cpus.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('cpus:index'),
+        ),
+        name='registration',
+    ),
     path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
